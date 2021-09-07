4deec908786a863eed832eba6273f46ad89388fc
Pearse Hutson <pearse.2008@gmail.com>
2021-09-07 11:22:00 -0500

    Should work

---
+++ Chapter 3 Exercises/Chapter 3 Exercises/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chapter_3_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            var num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
